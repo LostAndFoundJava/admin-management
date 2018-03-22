d76eb29b98c6fb04ab7a428eae10ba8db8d7260e
JackieChan <chendengqian@shhxzq.com>
2018-03-22 22:15:40 +0800

    热点新闻

---
+++ king-admin-java/src/main/java/com/oukingtim/web/news/NewsManagementController.java
@@ -0,0 +1,18 @@
+package com.oukingtim.web.news;
+
+/**
+ * <br>创建日期：2018/3/22
+ * <br>Copyright 2018　上海华信证券　All Rights Reserved</b>
+ *
+ * @author JackieChan</b>
+ * @version 1.****</b>
+ */
+
+public class NewsManagementController {
+
+    //新闻查询
+    //新闻编辑
+    //新闻发布
+    //新闻下线
+
+}
