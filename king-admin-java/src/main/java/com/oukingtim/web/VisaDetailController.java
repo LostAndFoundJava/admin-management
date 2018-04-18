e18ca41f02d0dae097b066f759dcf7115338006c
JackieChan <chendengqian@shhxzq.com>
2018-04-18 10:42:29 +0800

    优化visa detail controller

--- king-admin-java/src/main/java/com/oukingtim/web/VisaDetailController.java
+++ king-admin-java/src/main/java/com/oukingtim/web/VisaDetailController.java
@@ -1,16 +1,9 @@
 package com.oukingtim.web;
 
-import com.oukingtim.domain.File;
 import com.oukingtim.domain.VisaModel;
-import com.oukingtim.service.FileService;
 import com.oukingtim.service.VisaDetailService;
-import com.oukingtim.web.vm.ResultVM;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * <br>创建日期：2018/4/17
