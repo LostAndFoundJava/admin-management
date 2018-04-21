c482e9141ce8a8cd01450b156df49c96a8ed402f
thx-xf <1005364989@qq.com>
2018-04-21 17:56:13 +0800

    visa

---
+++ king-admin-angularjs/src/app/pages/visaupload/management/management.module.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+
+    angular.module('KingAdmin.pages.visaupload.management', [])
+        .config(routeConfig);
+
+    /** @ngInject */
+    function routeConfig($stateProvider) {
+        $stateProvider
+            .state('visa.management', {
+                url: '/management',
+                templateUrl: 'app/pages/visaupload/management/managementlist.html',
+                controller: 'visaUploadListCtrl',
+                controllerAs: 'kt',
+                title: '签证列表',
+                sidebarMeta: {
+                    order: 1,
+                },
+            })
+            .state('visa.management.add', {
+                url: '/add',
+                title: '新增签证',
+                views: {
+                    '@': {
+                        templateUrl: 'app/pages/visaupload/management/management.html',
+                        controller: 'visaUploadCtrl',
+                        controllerAs: 'kt',
+                    }
+                }
+            })
+            .state('visa.management.edit', {
+                url: '/edit/:id',
+                title: '编辑签证',
+                views: {
+                    '@': {
+                        templateUrl: 'app/pages/visaupload/management/management.html',
+                        controller: 'visaUploadCtrl',
+                        controllerAs: 'kt',
+                    }
+                }
+            })
+            .state('visa.management.view', {
+                url: '/view/:id?isView',
+                title: '查看签证',
+                views: {
+                    '@': {
+                        templateUrl: 'app/pages/visaupload/management/management.html',
+                        controller: 'visaUploadCtrl',
+                        controllerAs: 'kt',
+                    }
+                }
+            });
+    }
+
+})();
