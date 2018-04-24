21d0969826c3fc8ede2bca8f7eebbcca2917ed5b
thx-xf <1005364989@qq.com>
2018-04-24 14:14:39 +0800

    修改url

--- king-admin-angularjs/src/app/pages/excelupload/excelupload.module.js
+++ king-admin-angularjs/src/app/pages/excelupload/excelupload.module.js
@@ -3,13 +3,14 @@
 
     angular.module('KingAdmin.pages.excelupload', [
         'KingAdmin.pages.excelupload.excelupload',
+        'KingAdmin.pages.excelupload.flowsrc',
     ]).config(routeConfig);
 
     /** @ngInject */
     function routeConfig($stateProvider) {
         $stateProvider
             .state('excelupload', {
-                url: '/mgr/excel',
+                url: '/mgr/flowsrc',
                 template: '<ui-view></ui-view>',
                 abstract: true,
                 title: 'excel导入',
