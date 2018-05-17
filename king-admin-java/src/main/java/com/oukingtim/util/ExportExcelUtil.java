5d635fc24bd77c42459907fa38cbe108c7d998d8
thx-xf <1005364989@qq.com>
2018-05-17 13:56:25 +0800

    改成生产文件路径

--- king-admin-java/src/main/java/com/oukingtim/util/ExportExcelUtil.java
+++ king-admin-java/src/main/java/com/oukingtim/util/ExportExcelUtil.java
@@ -131,8 +131,8 @@ public class ExportExcelUtil {
                 datacell.setCellStyle(cellStyle);
             }
             //自定义文件存储路径
-            String localPath = "C:\\Users\\xufan\\Documents";//本地
-            String prodPath = Constants.EXCEL_BASE_PATH;//服务器
+            String localPath = "C:\\Users\\xufan\\Documents"; //本地
+            String prodPath = Constants.EXCEL_BASE_PATH; //服务器
             //发送响应流方法
             String excelName = "/" + UUID.randomUUID().toString() + ".xls";
             String creteFile = prodPath + excelName;//本地
