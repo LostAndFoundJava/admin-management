c3142faadb47ebe99b0c170a56eced666a00fd12
thx-xf <1005364989@qq.com>
2018-05-27 11:36:25 +0800

    导入数据来源记录日志

--- king-admin-angularjs/src/app/pages/excelupload/excelupload/excelupload.controller.js
+++ king-admin-angularjs/src/app/pages/excelupload/excelupload/excelupload.controller.js
@@ -4,14 +4,14 @@
     angular.module('KingAdmin.pages.excelupload.excelupload')
         .controller('ExcelUploadCtrl', ExcelUploadCtrl);
     /** @ngInject */
-    function ExcelUploadCtrl($scope, Upload, $timeout,toastr) {
+    function ExcelUploadCtrl($scope, Upload, $timeout, toastr, ExcelUploadService) {
         var kt = this;
         kt.files = [];
         $scope.uploadFiles = function (files) {
             if (files && files.length) {
                 angular.forEach(files, function (file) {
                     var wjzl = {};
-                    wjzl.name = file.name;
+                    wjzl.fileName = file.name;
                     kt.files.push(wjzl);
                 })
 
@@ -24,16 +24,26 @@
                     $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
                     // toastr.warning('progress: ' + $scope.progress + '% ' );
                 }).success(function (data, status, headers, config) {
-                    if(data && data.code == 500){
+                    if (data && data.code == 500) {
                         toastr.warning(data.msg);
                         $scope.progress = "0";
-                    }else{ toastr.success('导入成功');}
+                    } else {
+                        toastr.success('导入成功');
+                    }
                 }).error(function (data, status, headers, config) {
                     toastr.warning(data.msg);
                     $scope.progress = "0";
                 })
             }
         };
+
+        ExcelUploadService.getLogs({}, function (data) {
+            if (data) {
+                kt.files = data.result;
+            }
+
+        })
+
 
     }
 
